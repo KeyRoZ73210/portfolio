2cda253cd61c048da209a86ea0e95ab777a20fe2
KeyRoZ73210 <thomas.fleury73@gmail.com>
2026-05-10 22:05:37 +0200

    FEAT: création de l'architecture global

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function ProjectsPage() {
+  return (
+    <div>
+      <h1>Projects</h1>
+      <Link href="/projects/robinbernardmagici">
+        Robin Bernard Magici
+      </Link>
+    </div>
+  );
+}
---
+++ src/app/projects/robinbernardmagici/page.tsx
@@ -0,0 +1,22 @@
+// import Image from 'next/image';
+
+export default function RobinBernardMagici() {
+  return (
+    <div>
+      <h1>Robin Bernard Magici</h1>
+      <p>
+        Site web pour un magicien
+      </p>
+      <a href="https://robinbernardmagici.thomasfly.fr/" target="_blank"> Robinbernardmagici</a>
+      <br />
+      <a href="https://github.com/KeyRoZ73210/robinbernardmagici" target="_blank"> github</a>
+
+      {/* <Image
+        src="/images/robinbernardmagici/robinbernardmagici.webp"
+        alt="Robin Bernard Magici"
+        width={1920}
+        height={1080}
+      /> */}
+    </div>
+  );
+}
